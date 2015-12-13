b0f832bb04152723cb63cca3bddf22e253f22dbd
jseteny <jseteny@gmail.com>
2015-12-13 19:51:05 +0100

    2 links about possible how to

--- util/src/main/scala/hu/matan/calendar/util/saveload/MeetingSaver.scala
+++ util/src/main/scala/hu/matan/calendar/util/saveload/MeetingSaver.scala
@@ -11,10 +11,20 @@ object MeetingSaver {
 
   private var list: List[Meeting] = List[Meeting]()
 
+
+  http :// stackoverflow.com / questions / 32790957 / define - codec - for -recursive - data - structure
+
+  https :// github.com / scodec / scodec / tree / master / shared / src / test / scala / scodec / codecs
+
+
+  val codecm: Codec[Meeting] = scodec.codecs.lazily(scodec.codecs.string :: scodec.codecs.long :: scodec.codecs.long)
+
+  def codec: Codec[List[Meeting]] = scodec.codecs.lazily(scodec.codecs.list(codec)).as[List[Meeting]]
+
   def save(meeting: Meeting): Unit = {
     list = meeting :: list
     val out = new FileOutputStream("meeting-list.scodec")
-    val bitVector: BitVector = Codec[List[Meeting]].encode(list).require
+    val bitVector: BitVector = codec.encode(list).require
     out.write(bitVector.toByteArray)
   }
 
